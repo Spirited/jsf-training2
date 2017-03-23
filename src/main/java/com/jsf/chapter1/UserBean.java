9414814cbaa366089c6275059d72d4c66d284cce
Sergey Kompanets <sergey_kompanets@ukr.net>
2017-03-23 23:21:28 +0200

    Init project. Wrote chapter1

---
+++ src/main/java/com/jsf/chapter1/UserBean.java
@@ -0,0 +1,34 @@
+package com.jsf.chapter1;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("user")
+@SessionScoped
+public class UserBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private String password;
+	
+	public String getGreeting() {
+		if ( name == null ) return "";
+		if ( name.length() == 0 ) return "";
+		else return "Welcome to JSF + Ajax, " + name + "!";
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
