776f0e47bc5efa85b06589551ca1302c57f9df4e
Sergey Kompanets <sergey_kompanets@ukr.net>
2017-03-26 22:52:58 +0300

    Described Problem class

--- src/main/java/com/jsf/chapter3/Problem.java
+++ src/main/java/com/jsf/chapter3/Problem.java
@@ -6,5 +6,18 @@ import java.io.Serializable;
  * Created by spirited on 26.03.2017.
  */
 public class Problem implements Serializable {
-    
+    private String question;
+    private String answer;
+
+    public Problem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() { return question; }
+    public String getAnswer() { return answer; }
+
+    public boolean isCorrect(String response) {
+        return response.trim().equalsIgnoreCase(answer);
+    }
 }
