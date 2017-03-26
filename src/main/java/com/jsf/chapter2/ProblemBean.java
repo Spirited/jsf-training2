6713a2492e21ef04e14d55d816b2f7584e8da409
Sergey Kompanets <sergey_kompanets@ukr.net>
2017-03-26 16:37:57 +0300

    Described ProblemBean class

--- src/main/java/com/jsf/chapter2/ProblemBean.java
+++ src/main/java/com/jsf/chapter2/ProblemBean.java
@@ -1,10 +1,26 @@
 package com.jsf.chapter2;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by spirited on 26.03.2017.
  */
 public class ProblemBean implements Serializable {
+    private ArrayList<Integer> sequence;
+    private int solution;
 
+    public ProblemBean() {}
+
+    public ProblemBean(int[] values, int solution) {
+        for ( int i = 0; i < values.length; i++ )
+            sequence.add(i);
+        this.solution = solution;
+    }
+
+    public ArrayList<Integer> getSequence() { return sequence; }
+    public void setSequence(ArrayList<Integer> sequence) { this.sequence = sequence; }
+
+    public int getSolution() { return solution; }
+    public void setSolution(int solution) { this.solution = solution; }
 }
